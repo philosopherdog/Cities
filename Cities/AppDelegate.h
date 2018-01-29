00abd35c1a3d38d6314088c26ffe42daf9386403
steve thompson <steven.thompson.ca@gmail.com>
2018-01-29 09:48:17 -0500

    Initial Commit

---
+++ Cities/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cities
+//
+//  Created by steve on 2018-01-29.
+//  Copyright © 2018 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
